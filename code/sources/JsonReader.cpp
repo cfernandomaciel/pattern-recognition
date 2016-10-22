2d1e1d2c03acae63bee30e85f67b6cec5d20c610
cfernandomaciel <claudio.fernando@gmail.com>
2016-10-22 12:18:50 -0200

    partial impl

--- code/sources/JsonReader.cpp
+++ code/sources/JsonReader.cpp
@@ -20,18 +20,18 @@ namespace dynengines {
         qWarning() << val;
         QJsonDocument d = QJsonDocument::fromJson(val.toUtf8());
         QJsonObject sett2 = d.object();
-        QJsonValue value = sett2.value(QString("appName"));
+        QJsonValue value = sett2.value(QString("boxes"));
         qWarning() << value;
         QJsonObject item = value.toObject();
         qWarning() << tr("QJsonObject of description: ") << item;
 
         /* incase of string value get value and convert into string*/
-        qWarning() << tr("QJsonObject[appName] of description: ") << item["description"];
-        QJsonValue subobj = item["description"];
+        qWarning() << tr("QJsonObject[appName] of description: ") << item["width"];
+        QJsonValue subobj = item["width"];
         qWarning() << subobj.toString();
 
         /* incase of array get array and convert into string*/
-        qWarning() << tr("QJsonObject[appName] of value: ") << item["imp"];
+        qWarning() << tr("QJsonObject[appName] of value: ") << item["height5"];
         QJsonArray test = item["imp"].toArray();
         qWarning() << test[1].toString();
 
